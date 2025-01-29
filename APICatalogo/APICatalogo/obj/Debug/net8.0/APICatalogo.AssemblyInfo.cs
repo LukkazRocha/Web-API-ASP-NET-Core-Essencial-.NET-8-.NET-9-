b5b89c5b99273c9569e9aad19c83ce48d2437c53
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2025-01-29 14:01:03 -0300

    add dbContext

--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f4c7d7ad36acfca106367796f4581ebe9531378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d02839e3b6ce8341fc6ccf3afcf67bb824058d41")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
