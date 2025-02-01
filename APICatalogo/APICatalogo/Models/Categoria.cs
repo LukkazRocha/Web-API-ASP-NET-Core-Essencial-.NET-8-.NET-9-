044c9500401b59776b21d1c0e3f856a1c4e75a07
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2025-02-01 15:45:25 -0300

    add migração inicial

---
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models;
+
+[Table("Categoria")]
+public class Categoria
+{
+    public Categoria()
+    {
+        Productos = new Collection<Produto>();
+    }
+
+    [Key]
+    public int CatagoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto>? Productos { get; set; }
+}
